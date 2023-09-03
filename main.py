7834dac072726a73e54ce2147aff7fd217b34f7d
ddtamn <disanderlidsbun@gmail.com>
2023-09-03 18:00:44 +0800

    8000

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ if __name__ == "__main__":
     from os import getenv
 
     port = int(getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
